22546b0bac7bc0b29cac610699524cee04ff4d1f
Dimitar Malinov <malinovd02@gmail.com>
2022-12-01 11:46:38 +0200

    feat: Form Editor Class implemented

--- src/cars.ts
+++ src/cars.ts
@@ -1,1 +1,22 @@
+import { CarService } from "./data/CarService";
+import { Collection } from "./data/Collection";
+import { LocalStorage } from "./data/Storage";
+import { Editor } from "./dom/Editor";
+
+const storage = new LocalStorage();
+const collection = new Collection(storage, 'cars');
+const carService = new CarService(collection);
+
 console.log('cars');
+
+start();
+
+async function start() {
+    const form = document.getElementById('new-car') as HTMLFormElement;
+    const editor = new Editor(form, onSubmit);
+
+}
+
+async function onSubmit(data) {
+    carService.create(data);
+}
--- src/dom/Editor.ts
+++ src/dom/Editor.ts
@@ -1,3 +1,47 @@
-class Editor {
-    
+export class Editor {
+    constructor(
+        private form: HTMLFormElement,
+        private callback: (data: object) => any,
+    ) {
+        this.form.addEventListener('submit', this.onSubmit.bind(this));
+    }
+
+    setValue(name: string, value: any) {
+        const target = this.form.querySelector(`[name="${name}"]`);
+
+        if (target instanceof HTMLInputElement) {
+            if (target.type == 'checkbox') {
+                target.checked = value;
+            } else {
+                target.value = value;
+            }
+        } else if (target instanceof HTMLSelectElement || target instanceof HTMLTextAreaElement) {
+            target.value = value;
+        }
+    }
+
+    setValues(data: object) {
+        for (let [key, value] of Object.entries(data)) {
+            this.setValue(key, value);
+        }
+    }
+
+    clear() {
+        this.form.reset();
+    }
+
+    remove() {
+        this.form.remove();
+    }
+
+    attachTo(parent: Node) {
+        parent.appendChild(this.form);
+    }
+
+    private onSubmit(event: SubmitEvent) {
+        event.preventDefault();
+        const formData = new FormData(this.form);
+        const data = Object.fromEntries(formData);
+        this.callback(data);
+    }
 }
